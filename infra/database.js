2cbd7e278736c4b8feb92a44f34bd8a1e89e5cf8
Guilherme Brun Moraes <guibrunmoraes@hotmail.com>
2024-10-11 15:50:49 -0400

    database status endpoint and security fixes

--- infra/database.js
+++ infra/database.js
@@ -10,10 +10,15 @@ async function query(queryObject) {
   });
   await client.connect();
 
-  const result = await client.query(queryObject);
-  await client.end();
+  try {
+    const result = await client.query(queryObject);
 
-  return result;
+    return result;
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.end();
+  }
 }
 
 export default { query: query };
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  res.status(200).json({ status: "ok" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const versionPostgresDb = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const maxConnections = parseInt(databaseMaxConnections.rows[0].max_connections);
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dataBaseOpenConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const dataBaseOpenConnectionsValue = dataBaseOpenConnectionsResult.rows[0].count;
+
+  res.status(200).json({
+    update_at: updatedAt,
+    dependencies: {
+      database: {
+        version: versionPostgresDb,
+        max_connections: maxConnections,
+        open_connections: dataBaseOpenConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
